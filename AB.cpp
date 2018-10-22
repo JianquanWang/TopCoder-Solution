4b6da0ca09fb48e9b3262ab23f30989f5bc01c4c
JayWong <jwang438@uottawa.ca>
2018-10-22 16:37:36 -0400

    Update AB.cpp

--- AB.cpp
+++ AB.cpp
@@ -7,8 +7,6 @@ class AB{
     public:
     string createString(int N, int K){
         string s(N, 'A');
-		
-        if (K == 0) return "";
         if (N % 2 == 0){
         	if (K > N*N/4) return "";}
         else{
@@ -16,12 +14,13 @@ class AB{
     	for(int i=1; i<=(N-1)/2; i++){
             string res(i,'B');
             s.replace(N-i, i+1, res);
-            if (((N-i+1)*(i-1) < K) && (K<= (N-i)*i)){
+            if (((N-i+1)*(i-1) < K) && (K <= (N-i)*i)){
            		int tempK = K - (N-i+1)*(i-1);
-                s[N-i] = 'A';
-                s[tempK] = 'B';
-            }    
-         
+                if (tempK!=0){
+                	s[N-i] = 'A';
+                	s[tempK] = 'B';
+               		 break;}
+            }
     	}
         return s;
    }
